715c7e0a865dd86bb3daa68f274d60d7fae45230
edrickwong <wong.edrick@gmail.com>
2016-03-15 13:50:23 -0700

    Added moderately useful comments

--- src/main.c
+++ src/main.c
@@ -1,27 +1,41 @@
 #include <pebble.h>
 
+//Pebble UI is like stacks of Windows
+//Main Window that everything will sit ontop of
 static Window *s_main_window;
+//Time layer where the time will be displayed
 static TextLayer *s_time_layer;
-
+//Background layer where the FRC logo sits
 static BitmapLayer *s_background_layer;
+//Bitmap variable for the FRC logo image
 static GBitmap *s_background_bitmap;
-
+//Font for the time
 static GFont s_time_font;
 
-static void update_time() {
 
+//function for updating the time
+static void update_time() {
+//This grabs the time from the system as a blob and sets it to tick_time
 time_t temp = time(NULL); 
 struct tm *tick_time = localtime(&temp);
 
 // Write the current hours and minutes into a buffer
 static char s_buffer[8];
+//This line here should actually force the time to display in 24H, will need to fix that
 strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ? "%H:%M" : "%I:%M", tick_time);
-  
+//Sets the text in the textlayer to whatever the time is
 text_layer_set_text(s_time_layer, s_buffer);
 }
 
+//Tick handler that will be invoked every time the 'minute' changes
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
-  
+
+/*************************************************************************************/
+/* Key and Value pairs for all the teams**********************************************/
+/* This will be used by a dictionary later********************************************/
+/* Add team by adding a new KEY_TEAM_[team number] and *VALUE_[team number] pair here*/
+/*************************************************************************************/
+
 static const uint32_t KEY_TEAM_254 = 254;
 static const char *VALUE_254 = "The Cheesy Poofs";
   
@@ -31,9 +45,6 @@ static const char *VALUE_610 = "The Coyotes";
 static const uint32_t KEY_TEAM_746 = 746;
 static const char *VALUE_746 = "Gearheads";
   
-static const uint32_t KEY_TEAM_854 = 854;// changed this PK
-static const char *VALUE_854 = "Iron Bears";
-
 static const uint32_t KEY_TEAM_857 = 857;
 static const char *VALUE_857 = "Superior Roboworks";
   
@@ -76,13 +87,24 @@ static const char *VALUE_2013 = "Cybergnomes";
 static const uint32_t KEY_TEAM_2056 = 2056;
 static const char *VALUE_2056 = "OP Robotics";
 
-// Calculate the buffer size that is needed for the final Dictionary:
+
+
+/*************************************************************************************/
+/* Calculate the buffer size that is needed for the final Dictionary******************/
+/* uint8_t key_count should be the number of teams in the list ***********************/
+/*************************************************************************************/
+
 const uint8_t key_count = 17;
 
+  
+/*************************************************************************************/
+/* This calculates the size of the dictionary, based on the KEY and VALUE pairs above*/
+/* To add a new team, add 'strlen(VALUE_[team number]) + 1' to the list of params*****/  
+/*************************************************************************************/
+  
 const uint32_t size = dict_calc_buffer_size(key_count, strlen(VALUE_610) + 1,
                                            strlen(VALUE_254) + 1,
                                            strlen(VALUE_746) + 1,
-					strlen(VALUE_854)+1,
                                            strlen(VALUE_857) + 1,
                                            strlen(VALUE_907) + 1,
                                            strlen(VALUE_919) + 1,
@@ -108,11 +130,14 @@ DictionaryIterator iter;
 
 // Begin:
 dict_write_begin(&iter, buffer, sizeof(buffer));
-
+/*************************************************************************************/
+/* This adds a KEY and VALUE pair to the dictionary***********************************/
+/* Add team based by appending dict_write_cstring(&iter, KEY_TEAM_[team number], VALUE_[team number]);*/
+/*************************************************************************************/
+  
 dict_write_cstring(&iter, KEY_TEAM_254, VALUE_254);  
 dict_write_cstring(&iter, KEY_TEAM_610, VALUE_610);
 dict_write_cstring(&iter, KEY_TEAM_746, VALUE_746);
-dict_write_cstring(&iter, KEY_TEAM_854, VALUE_854);//changed this PK
 dict_write_cstring(&iter, KEY_TEAM_857, VALUE_857);
 dict_write_cstring(&iter, KEY_TEAM_907, VALUE_907);
 dict_write_cstring(&iter, KEY_TEAM_919, VALUE_919);
@@ -126,50 +151,67 @@ dict_write_cstring(&iter, KEY_TEAM_1334, VALUE_1334);
 dict_write_cstring(&iter, KEY_TEAM_1547, VALUE_1547);
 dict_write_cstring(&iter, KEY_TEAM_1815, VALUE_1815);
 dict_write_cstring(&iter, KEY_TEAM_2013, VALUE_2013);
-dict_write_cstring(&iter, KEY_TEAM_2056, VALUE_2056);//changed this PK
+dict_write_cstring(&iter, KEY_TEAM_2056, VALUE_2056);
 
 // End:
-  
+
+//Calculates the final size of the dictionary
 const uint32_t final_size = dict_write_end(&iter);
-
+//This sets up a tuple that will be used to iterate through the dictionary
 Tuple *tuple = dict_read_begin_from_buffer(&iter, buffer, final_size);
-
+//Logic for figuring out if the time matches a team number
 while (tuple) {
-  
+  //A really big buffer for the team name
   static char s_buffer[64];
   
   //snprintf(s_buffer, sizeof(s_buffer), "%s", tuple->value->cstring);
   
+  //Rips the current time measurement into hours and minutes
+  //Multiples the hour by 100 to shift it to the correct digit
+  //and then adds it to the minutes to form the correct FRC four digit team number
+  //For example, if the time is 11:14, then this takes the 11 (hours), multiplies it by 100 to get 1100
+  //and adds it to 14 (minutes) to get 1114, which is compared to all the keys in the dictionary (team numbers)
   if(tick_time->tm_hour*100+tick_time->tm_min == (int)tuple->key){
-    
+    //Debug stuff
     APP_LOG(APP_LOG_LEVEL_DEBUG, "this ran");
-    
+    //This writes the team name to the really big string buffer
     snprintf(s_buffer, sizeof(s_buffer), "%s", tuple->value->cstring);
-    
+    //More debug stuff
     APP_LOG(APP_LOG_LEVEL_DEBUG, "this ran 2");
-    
+    //This sets the font to a smaller sized font relative to the time font
+    //Because teams have long names
     text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
+    //This sets the text to the team name
     text_layer_set_text(s_time_layer, s_buffer);
-    
+    //Even more debug stuff
     APP_LOG(APP_LOG_LEVEL_DEBUG, "this ran 3");
     
-    
+    //Vibrates if time == team number
     vibes_long_pulse();
+    //Breaks out of the while loop, do it stops when the correct team is found
+    //and doesn't continue checking the rest of the dictionary
     break;
 
   }else{
+    //If time doesn't match any team numbers, just updates time normally
+    
+    //This resets the time font back to normal
     text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
     update_time();
+    //Debug stuff
     APP_LOG(APP_LOG_LEVEL_DEBUG, "this did no run");
   }
+  //Goes to the next dictionary item
   tuple = dict_read_next(&iter);
   }
   
 }
 
+//This function loads everything into the base window
 static void main_window_load(Window *window) {
   // Get information about the Window
   Layer *window_layer = window_get_root_layer(window);
+  //Finds the boundary for the Window, which is pretty much the size of the screen in this case
   GRect bounds = layer_get_bounds(window_layer);
 
   // Create GBitmap
@@ -180,7 +222,9 @@ static void main_window_load(Window *window) {
 
   // Set the bitmap onto the layer and add to the window
   bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
+  //GCompSet is the param that sets the transparency in a png
   bitmap_layer_set_compositing_mode(s_background_layer, GCompOpSet);
+  //Adds the png to the background layer
   layer_add_child(window_layer, bitmap_layer_get_layer(s_background_layer));
 
   // Create the TextLayer with specific bounds
@@ -188,11 +232,19 @@ static void main_window_load(Window *window) {
       GRect(0, /*PBL_IF_ROUND_ELSE(58, 52)*/ 110, bounds.size.w, 150));
 
   // Improve the layout to be more like a watchface
+  
+  //This makes the background for the text layer clear
+  //So the base Window background can show through
   text_layer_set_background_color(s_time_layer, GColorClear);
+  //Time text colour
   text_layer_set_text_color(s_time_layer, GColorRed);
+  //Default placeholder/backfall if time doesn't show up
   text_layer_set_text(s_time_layer, "ballsohard");
+  //Centers time text
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+  //Sets time text font
   text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+  //This wraps the text for the team names if it is too long
   text_layer_set_overflow_mode(s_time_layer, GTextOverflowModeWordWrap);
   //text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
   // Create GFont
@@ -205,6 +257,7 @@ static void main_window_load(Window *window) {
   layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
 }
 
+//This function is needed when the watchface is exited to free up resources on the watch
 static void main_window_unload(Window *window) {
   // Destroy TextLayer
   text_layer_destroy(s_time_layer);
@@ -219,6 +272,7 @@ static void main_window_unload(Window *window) {
   bitmap_layer_destroy(s_background_layer);
 }
 
+//Initialization/ setup function
 static void init() {
   // Create main Window element and assign to pointer
   s_main_window = window_create();
@@ -242,11 +296,13 @@ static void init() {
   tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
 }
 
+//Teardown function
 static void deinit() {
   // Destroy Window
   window_destroy(s_main_window);
 }
 
+//Main Function
 int main(void) {
   init();
   app_event_loop();
